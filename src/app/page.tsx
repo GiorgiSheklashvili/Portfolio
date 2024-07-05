7a31a9cbc56eb4c75376c68d6e560b1741455554
Giorgi <gsheklashvili@association.ge>
2024-07-05 18:14:58 +0400

    Home page updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,20 @@
-export default function Home() {
+import React from 'react';
+import Introduction from './homepage/Introduction';
+
+
+const HomePage: React.FC = () => {
   return (
+
     <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
-      </h1>
-      <p className="mb-4">{`I specialize in Spring Boot development and advocate for microservices architecture. With a primary focus on the fintech sector, I currently serve as a Software Architect for an Open Banking project in Georgia. `}</p>
-      <p className="mb-4">University of Tartu Alumni: <a href="https://figshare.com/articles/thesis/Multi-Factor_Authentication_MFA_on_a_Blockchain-based_Decentralised_Trust_Network_With_Customizable_Challenges/12578696" style={{ color: 'blue' }}>Master's thesis</a></p>
-      <div className="my-8">{/* <BlogPosts /> */}</div>
+      <div>
+        <Introduction />
+        
+        <p className="mb-4">University of Tartu Alumni: <a href="https://figshare.com/articles/thesis/Multi-Factor_Authentication_MFA_on_a_Blockchain-based_Decentralised_Trust_Network_With_Customizable_Challenges/12578696" style={{ color: 'blue' }}>Master's thesis</a></p>
+        <div className="my-8">{/* <BlogPosts /> */}</div>
+      </div>
+
     </section>
   );
-}
+};
+
+export default HomePage;
